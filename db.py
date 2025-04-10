44d8a20af71bd26b152abc824b8e8b53cd3b59dd
Abhuday Singh <147344196+ZaGrayWolf@users.noreply.github.com>
2025-04-10 20:43:27 +0530

    Update db.py

--- db.py
+++ db.py
@@ -5,8 +5,8 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="MySQL_Abhuday",
-        database="DBMS_Project"
+        password="******",
+        database="*****"
     )
 
 def hash_password(password):
